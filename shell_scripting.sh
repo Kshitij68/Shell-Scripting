1c4fcad7a84730e1ce296cb697d21579c1ca7920
Kshitij68 <k.mathur68@gmail.com>
2018-12-14 03:57:08 +0530

    Lecture 1 - 6 Notes

---
+++ shell_scripting.sh
@@ -0,0 +1,16 @@
+All UNIX commands are lower case
+
+who am I => Time when I logged in
+
+pwd => Present Working Directory
+
+cal => Calendar of current month
+cal 07 2006 => Calendar of July 2006
+
+date => Current date (Sun May 20 06:48:21 2018)
+date '+DATE %m-%y%nTIME:%H:%M:%S' => Current date in custom format
+
+touch test1 test2 test3 => Creates three empty test files called test1, test2, test3
+
+ 
+
