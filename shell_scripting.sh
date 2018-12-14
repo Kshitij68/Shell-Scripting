3dd93e672b87f0e20980482bf2b1e3dd3e5aa03a
Kshitij68 <k.mathur68@gmail.com>
2018-12-14 15:30:21 +0530

    Shell Scripting add notes

--- shell_scripting.sh
+++ shell_scripting.sh
@@ -1,16 +1,49 @@
-All UNIX commands are lower case
+# All UNIX commands are lower case
 
-who am I => Time when I logged in
+# Know time when logged in
+who am I
 
-pwd => Present Working Directory
+# Get the present working directory
+pwd
 
+# Calendar
+# Get the calendar of current month
 cal => Calendar of current month
-cal 07 2006 => Calendar of July 2006
+# Get the calendar for July 2006
+cal 07 2006
 
-date => Current date (Sun May 20 06:48:21 2018)
-date '+DATE %m-%y%nTIME:%H:%M:%S' => Current date in custom format
+# Date
+# Get the current date (Sun May 20 06:48:21 2018) 
+date
+# Get the current date in custom format
+date '+DATE %m-%y%nTIME:%H:%M:%S'
 
-touch test1 test2 test3 => Creates three empty test files called test1, test2, test3
+# Create a new empty files
+touch test1 test2 test3
 
- 
+# Create a new directory
+mkdir filename
+mkdir Documents/filename
 
+# cat
+# To open a text file. Save using Ctrl + D
+cat > test
+# To view the contents of the file
+cat < test
+# To combine two files into one. Text of raad + test goes into sample
+cat rand test > sample
+
+# mv
+# To rename a file
+mv forest_gump the_green_mile
+
+# rm
+# To remove a file 'the_green_mile'
+rm the green_mile
+# To remove a directory 'Documents'
+rm -r Documents
+# To remove  directory 'Documents'
+rmdir Documents
+
+# Video 7
+# https://www.youtube.com/watch?v=8CEpUQCJKhc&list=PL7B7FA4E693D8E790&index=7
