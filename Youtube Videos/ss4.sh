9834c5ffa3f26ee8391c1467e0aef8c2583179f3
Kshitij68 <k.mathur68@gmail.com>
2018-12-18 19:38:01 +0530

    Lec 20 - Positional Paramters II

---
+++ Youtube Videos/ss4.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This file takes a file name as an argument and renames it
+# Shell by default stores the numbers as string
+# $1 : Positional Paramter which we provide as an argument to the script
+echo "Provde a filename"
+read name
+mv $1 $name
+
+# Run as bash ss4.sh <input_file_name>
