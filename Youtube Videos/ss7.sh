83edf7fed3a3f1c7ff1bd1829980d50d32d0cad0
Kshitij68 <k.mathur68@gmail.com>
2018-12-18 19:39:27 +0530

    Lec 23 - Math Expressions

---
+++ Youtube Videos/ss7.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set My name is Kshitij
+echo $# # Returns total number of positional parameters
+
+# bash ss7.sh *
+# * character will make the script think that positional parameters should be passed
