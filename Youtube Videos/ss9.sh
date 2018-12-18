5d6280f62c2791fb29da01e6540127eb9c69b12f
Kshitij68 <k.mathur68@gmail.com>
2018-12-18 19:40:00 +0530

    Lec 25 - Math on Real Numbers

---
+++ Youtube Videos/ss9.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Operator Precedence: There are five operators:
+# Addition : +
+# Subtraction : -
+# Multiplication : *
+# Division : /
+# Remainder : %
+a=30
+b=15
+c=2
+d=5
+echo `expr $a \* \( $b + $c \) / $d`
