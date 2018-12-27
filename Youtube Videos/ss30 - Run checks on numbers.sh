54c53951a7605e7a891d12ce4ed7f37f1adb681d
Kshitij68 <k.mathur68@gmail.com>
2018-12-27 19:22:46 +0530

    lec30

---
+++ Youtube Videos/ss30 - Run checks on numbers.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo -e "Enter a number between 10 and 20: \c"
+read num
+if [ $num -lt 10 ]
+then
+    echo "That was under the belt partner"
+elif [ $num -gt 20 ]
+then
+    echo "That went over my head."
+else
+    echo "Now you are making sense!"
+fi
+# [ $num -ne 10 ] # Not Equal
