f09db1ab9f72b26135e71a6f56a04fb6bbefca87
Kshitij68 <k.mathur68@gmail.com>
2019-01-20 02:23:45 +0530

    Exercise 7

--- Hackerrank exercises/Exercise 7  - Getting started with Conditionals.sh
+++ Hackerrank exercises/Exercise 7  - Getting started with Conditionals.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+read char
+if [ "$char" = "Y" ] || [ "$char" = "y" ]
+then
+    echo "YES"
+elif [ "$char" = "N" ] || [ "$char" = "n" ]
+then
+    echo "NO"
+else
+    echo "MIND-BLOWN"
+fi
